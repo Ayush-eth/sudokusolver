50bab632180a1470cb1747c6b796ae4391b9f0eb
Ayush <91387353+Ayush-eth@users.noreply.github.com>
2022-07-04 19:21:11 +0530

    Update settings.py

--- SudokuSolver/settings.py
+++ SudokuSolver/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-0eh22m61!%!er#py3bp0gdh-*$j6=7bn54$*b$no)*mb7%vrn3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['localhost','127.0.0.1', 'solvethesudoku.herokuapp.com']
 
