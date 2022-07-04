ad893572fd0a929bb73c274516461c6945ec1bb2
Ayush <91387353+Ayush-eth@users.noreply.github.com>
2022-07-04 18:58:21 +0530

    Update urls.py

--- SudokuSolver/SudokuSolver/urls.py
+++ SudokuSolver/SudokuSolver/urls.py
@@ -23,5 +23,7 @@ from. import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index,name='index'),
-    path('result', views.solve,name='index')
+    path('result', views.solve,name='index'),
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ]
